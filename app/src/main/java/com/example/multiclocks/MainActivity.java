2f9eaec84df2be57207cdc2c0b50401cb4433ee2
cipher <assassin18008198@gmail.com>
2024-10-31 10:07:04 +0700

    Tạo tính năng chọn timezone.

--- app/src/main/java/com/example/multiclocks/MainActivity.java
+++ app/src/main/java/com/example/multiclocks/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.multiclocks;
 
+import static android.text.TextUtils.substring;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.widget.TextClock;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +28,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Spinner spinner = findViewById(R.id.spinner2);
+        String[] listTimeZones = new String[]{"UTC-12:00", "UTC-11:00", "UTC-10:00", "UTC-09:30", "UTC-09:00", "UTC-08:00",
+                "UTC-07:00", "UTC-06:00", "UTC-05:00", "UTC-04:00", "UTC-03:30", "UTC-03:00", "UTC-02:00", "UTC-01:00", "UTC+00:00",
+                "UTC+01:00", "UTC+02:00", "UTC+03:00", "UTC+03:30", "UTC+04:00", "UTC+04:30", "UTC+05:00", "UTC+05:30", "UTC+05:45",
+                "UTC+06:00", "UTC+06:30", "UTC+07:00", "UTC+08:00", "UTC+08:45", "UTC+09:00", "UTC+09:30", "UTC+10:00", "UTC+10:30",
+                "UTC+11:00", "UTC+12:00", "UTC+12:45", "UTC+13:00", "UTC+14:00"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, listTimeZones);
+        TextView textClock = findViewById(R.id.textTime);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String timeZone = listTimeZones[i];
+                String strZoneOffset = timeZone.substring(3);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+                ZoneOffset zoneOffset = ZoneOffset.of(strZoneOffset);
+                OffsetDateTime now = OffsetDateTime.now(zoneOffset);
+                String formattedTime = now.format(formatter);
+                textClock.setText(formattedTime);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
 }
